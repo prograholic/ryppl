323b61bca3b70458f28f5ef7198de276eb6bc6ee
Alexey Kutumov <alexey.kutumov@gmail.com>
2013-01-11 17:40:28 +0700

    Fixed multiple includes and defines parameters

--- cmake/Modules/RypplQuickbook.cmake
+++ cmake/Modules/RypplQuickbook.cmake
@@ -10,33 +10,32 @@ ryppl_find_and_use_package(Quickbook)
 
 # convert qbk to xml via quickbook
 function(quickbook_to_boostbook qbkFile xmlFile)
-
   cmake_parse_arguments(ryppl_qbk
     "" # no options
     "INDENT;LINEWIDTH" # one value params
     "INCLUDES;DEFINES" # multi value params
     ${ARGN}
-    )
+  )
 
-    if(${ryppl_qbk_INDENT})
-      set(RYPPL_QBK_INDENT_ARG "--indent ${ryppl_qbk_INDENT}")
-    endif()
+  if(${ryppl_qbk_INDENT})
+    set(RYPPL_QBK_INDENT_ARG "--indent ${ryppl_qbk_INDENT}")
+  endif()
 
-    if(${ryppl_qbk_LINEWIDTH})
-      set(RYPPL_QBK_LINEWIDTH_ARG "--linewidth ${ryppl_qbk_LINEWIDTH}")
-    endif()
+  if(${ryppl_qbk_LINEWIDTH})
+    set(RYPPL_QBK_LINEWIDTH_ARG "--linewidth ${ryppl_qbk_LINEWIDTH}")
+  endif()
 
-    if(${ryppl_qbk_INCLUDES})
-      set(RYPPL_QBK_INCLUDES_ARG "-I${ryppl_qbk_INCLUDES}")
-    endif()
+  foreach(inc_entry ${ryppl_qbk_INCLUDES})
+    set(RYPPL_QBK_INCLUDES_ARG  ${RYPPL_QBK_INCLUDES_ARG} "-I${inc_entry}")
+  endforeach()
 
-    if(${ryppl_qbk_DEFINES})
-      set(RYPPL_QBK_DEFINES_ARG "-D${ryppl_qbk_DEFINES}")
-    endif()
+  foreach(def_entry ${ryppl_qbk_DEFINES})
+    set(RYPPL_QBK_DEFINES_ARG  ${RYPPL_QBK_DEFINES_ARG} "-D${def_entry}")
+  endforeach()
 
   add_custom_command(
     COMMAND quickbook --input-file ${qbkFile} --output-file ${xmlFile} ${RYPPL_QBK_INCLUDES_ARG} ${RYPPL_QBK_DEFINES_ARG} ${RYPPL_QBK_INDENT_ARG} ${RYPPL_QBK_LINEWIDTH_ARG}
     OUTPUT ${xmlFile}
     COMMENT "converting quickbook file (${qbkFile}) to boostbook xml ..."
-    )
+  )
 endfunction(quickbook_to_boostbook)
