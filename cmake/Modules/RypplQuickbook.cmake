3e9bf4b2750a4bd074ba238ccfee45a6a6215811
Alexey Kutumov <alexey.kutumov@gmail.com>
2013-01-12 13:48:39 +0700

    Added support for hidden quickbook parameter --xinclude-base

--- cmake/Modules/RypplQuickbook.cmake
+++ cmake/Modules/RypplQuickbook.cmake
@@ -12,16 +12,20 @@ ryppl_find_and_use_package(Quickbook)
 function(quickbook_to_boostbook qbkFile xmlFile)
   cmake_parse_arguments(ryppl_qbk
     "" # no options
-    "INDENT;LINEWIDTH" # one value params
+    "INDENT;LINEWIDTH;XINCLUDE_BASE" # one value params
     "INCLUDES;DEFINES" # multi value params
     ${ARGN}
   )
 
-  if(${ryppl_qbk_INDENT})
+  if (ryppl_qbk_XINCLUDE_BASE)
+    set(RYPPL_QBK_XINCLUDE_BASE_ARG --xinclude-base ${ryppl_qbk_XINCLUDE_BASE})
+  endif()
+
+  if(ryppl_qbk_INDENT)
     set(RYPPL_QBK_INDENT_ARG "--indent ${ryppl_qbk_INDENT}")
   endif()
 
-  if(${ryppl_qbk_LINEWIDTH})
+  if(ryppl_qbk_LINEWIDTH)
     set(RYPPL_QBK_LINEWIDTH_ARG "--linewidth ${ryppl_qbk_LINEWIDTH}")
   endif()
 
@@ -34,7 +38,7 @@ function(quickbook_to_boostbook qbkFile xmlFile)
   endforeach()
 
   add_custom_command(
-    COMMAND quickbook --input-file ${qbkFile} --output-file ${xmlFile} ${RYPPL_QBK_INCLUDES_ARG} ${RYPPL_QBK_DEFINES_ARG} ${RYPPL_QBK_INDENT_ARG} ${RYPPL_QBK_LINEWIDTH_ARG}
+    COMMAND quickbook --debug --input-file ${qbkFile} --output-file ${xmlFile} ${RYPPL_QBK_XINCLUDE_BASE_ARG} ${RYPPL_QBK_INCLUDES_ARG} ${RYPPL_QBK_DEFINES_ARG} ${RYPPL_QBK_INDENT_ARG} ${RYPPL_QBK_LINEWIDTH_ARG}
     OUTPUT ${xmlFile}
     COMMENT "converting quickbook file (${qbkFile}) to boostbook xml ..."
   )
