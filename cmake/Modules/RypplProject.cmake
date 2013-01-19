fe2a8a4f44fa28465000d655d4bf295ddcb0d779
Alexey Kutumov <alexey.kutumov@gmail.com>
2013-01-19 16:59:47 +0700

    Added support for automatic per-project dependencies. Now one can adds all necessary dependencies for project and if all requirements are met, than mark project as valid, otherwise project marked as invalid, and all dependend projects became invalid too. For example if you does not have installed MPI than configuration script disables building of boost_mpi, graph_parallel.

---
+++ cmake/Modules/RypplProject.cmake
@@ -0,0 +1,68 @@
+# Function for declaring project with dependencies
+#
+# Function declares new project,
+#  searches and uses every depended and recommended project
+#
+# If all requirements are met, this function declares variable
+#  RYPPL_${PROJECT_NAME}_VALID
+#
+# Signature:
+#   ryppl_project(<proj_name> [DEPENDS <project list>] [RECOMMENDS <project list>])
+#
+# Parameters:
+#  proj_name - required parameter, sets the name of project
+# TODO add others param description
+#=============================================================================
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#=============================================================================
+
+include(Ryppl)
+include(CMakeParseArguments)
+
+
+if(__RYPPL_PROJECT_INCLUDED)
+  return()
+endif()
+set(__RYPPL_PROJECT_INCLUDED TRUE)
+
+macro(ryppl_project proj_name)
+  project(${proj_name})
+
+  cmake_parse_arguments(var
+    "" # no options
+    "" # one value params
+    "DEPENDS;RECOMMENDS" # multi value params
+    ${ARGN}
+  )
+
+
+  set(RYPPL_${proj_name}_VALID 1)
+
+
+
+  foreach(proj ${var_DEPENDS})
+    ryppl_find_and_use_package(${proj})
+    if (NOT ${RYPPL_INITIAL_PASS})
+      if (NOT ${proj}_FOUND)
+
+        # fix compatibility issues,
+        # for example PythonLibs package scrip set PYTHONLIBS_FOUND
+        # instead of PythonLibs_FOUND
+        # so check uppercase value if normal value is not set
+        string(TOUPPER "${proj}_FOUND" proj_compat_found)
+
+        if (NOT ${proj_compat_found})
+          set(RYPPL_${proj_name}_VALID 0)
+          list(APPEND missing_dependend_projects ${proj})
+        endif()
+      endif()
+    endif()
+  endforeach()
+
+  if (NOT ${RYPPL_${proj_name}_VALID})
+    message(WARNING "project ${proj_name} cannot be properly configured, missing modules: ${missing_dependend_projects}")
+  endif()
+
+endmacro()
